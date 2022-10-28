0969d84cac63f41c695ea27fc38452853e72a8c1
Adnan Qasim <shiekh111aq@gmail.com>
2022-10-28 19:21:29 +0530

    added user creation

--- login.py
+++ login.py
@@ -1,8 +1,7 @@
 import re
-from turtle import delay
-from unicodedata import unidata_version
 import time
-def sing_up():
+
+def register():
     fname =input("Enter full name: ")
     uname= input("Enter Username: ")
     valid_uname= re.match(r"[a-z]+(\d+)[a-z]*(\d*)$",uname)
@@ -10,7 +9,13 @@ def sing_up():
         print(valid_uname.group()+ " is a valid Username ")
     else:
         print(uname+" is invalid Username\nCharacter must start with lowercase alphabet [a-z] and Should have atleast 1 digit [0-9] and Should be 6 Characters long\nRetry...")
-        sing_up()
+        register()
+    e_mail=input("Enter Valid email address: ")
+    if re.search(r'(\w{3,})@([a-z]+).([a-z]+)',e_mail):
+        print("Valid Email Address...")
+    else:
+        print("Invalid Email....\nRETRY")
+        register()
     passw=input("Enter Valid Password: ")
     cnf_pass=input("Confirm the Password: ")
     if re.search(r"[a-z]+",passw) and re.search(r"[A-Z]+",passw) and re.search(r"(\d+)",passw) and len(passw)>=8: 
@@ -19,12 +24,35 @@ def sing_up():
             time.sleep(1)
             print("...processing.....")
             time.sleep(2)
-            print("Account Created Successfully ;-)")
+            cr_acc(fname,uname,e_mail,passw)
 
         else: 
             print("Passwoed Valid but Doesn't Matches")
+            register()
     else:
         print("Invalid Password\nPassword must contain atleast a lowercase & UPPERCASE Alphabet [A-Z,a-z] and a Digit [0-9]...\nRetry...")
-        sing_up()
+        register()
 
-sing_up()
+
+def cr_acc(name,user,mail,passw):
+    acc_list=dict()
+    same_m=False
+    same_u=False
+    if len(acc_list)>0:
+        for i,j in acc_list:
+            if i==user:
+                same_u=True
+            if j[2]==mail:
+                same_m=True
+    if same_m==False :
+        if same_u==False:
+            acc_list[user]=[name,mail,passw]
+            print("Account with \nUsername: "+user+"\nEmail: "+mail+"\nPassword: "+passw+"\nHas been Created Successfully ;)")
+        else:
+            print("Username "+user+" Is Already in Use...\nRetry...")
+            register()
+    else:
+        print("Email "+mail+" Is Already in Use...\nRetry...")
+        register()
+    print(acc_list)
+register()
